2d9406842137d03dd1a1f9a3808174f13487a8be
jack_mcrider <jack_mcrider@8e49c7eb-f95a-0410-904b-fd23379f18b6>
2010-12-18 17:15:58 +0000

    added memo.c - just a to-do list
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/jackbot/trunk@20 8e49c7eb-f95a-0410-904b-fd23379f18b6

---
+++ dev/src/mods/memo.c
@@ -0,0 +1,48 @@
+#include "../lib/libjackbot.h"
+
+void memo(struct _Nfos_ *nfos);
+
+struct _Mods_ _mod_info = 
+{                        
+  NULL,                 
+  &memo,               
+  "Memo",             
+  "!memo",           
+  "JOIN",           
+  ""              
+};               
+
+/*
+ * This mod is for writing a message to someone who is not online atm.
+ * You (your nickname might be 'sergiorey') just write
+ *   !memo JacKMcRiDER The pope is a jew!? O_o
+ * 
+ * When 'JacKMcRiDER' is joining the channel, the bot will get
+ *   :JacKMcRiDER JOIN #foobar
+ *
+ * The bot searches through the memo file and finds the saved message. So he'll send
+ *   NOTICE sergiorey :MEMO by sergiorey from 18.12.2010, 17h47: The pope is a jew!? O_o
+ */
+void memo(struct _Nfos_ *nfos)
+{
+  if( strcmp( nfos->sender->command, nfos->mods->server_cmd ) ) // if someone wrote "!memo" in a channel (so you've to record one)
+  {
+    // record memo
+      // get channel
+      // get receiver nickname
+      // get sender nickname
+      // get memo message
+      // get date and time
+      // write to memo file
+      // answer "memo saved" to sender
+  }else // if someone joined a channel
+  {
+    // search and push memo
+      // get channel
+      // get nickname of joined user
+      // open memo file
+      // search for joined users nickname
+      // parse date, time, nick and memo message to "NOTICE [joined_nick/receiver_nick] :MEMO by [sender_nick] from [date], [time]: [memo]
+      // delete memo from memo file
+  }
+}
